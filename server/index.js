26d440d2251a9c2ed95702cc0fde5bedb61429c7
fonsecaBarreto <lucasbfonte@gmail.com>
2023-06-18 22:51:45 -0300

    feat: new http routes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const { csvStringToJson } = require("./parsers");
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.text())
 
-const metrics = [];
+const metrics = [{ timestamp: 0, humidity: 90, temperature: 36 }];
 
-app.post('/csv', (req, res) => {
-  console.log(new Date().toLocaleString(), " - data received:\n");
-  const json = csvStringToJson(req.body);
-  const result = (JSON.parse(json)[0]);
+app.get('/', (req, res) => {
+  return res.json("ok");
+})
+
+app.post('/json', (req, res) => {
+  console.log(topic, new Date().toLocaleDateString()," : " , payload.toString());
+  const dto = JSON.parse(payload.toString());
+  dto.timestamp = new Date();
   metrics.push(result);
   return res.send("ok");
 })
 
 app.get('/metrics', (req, res) => {
   let list = [ ...metrics];
-  list.sort(function(a, b){return b.timestamp - a.timestamp}); 
-  return res.json(list[0]);
+  return res.json(list[list.length - 1]);
 })
 
 app.listen(port, () => {
