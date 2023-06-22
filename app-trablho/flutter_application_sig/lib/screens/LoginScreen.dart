49fb347ce746ccf25cb1418f55378330064c1179
fonsecaBarreto <lucasbfonte@gmail.com>
2023-06-22 18:21:47 -0300

    feat: login screen

--- app-trablho/flutter_application_sig/lib/screens/LoginScreen.dart
+++ app-trablho/flutter_application_sig/lib/screens/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import '../controllers/homeController.dart';
+import 'package:flutter_application_sig/repositories/global_repository.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,15 +9,66 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
-  void initState() {}
+  final TextEditingController _emailController = TextEditingController();
+
+  Future<void> _submitForm(GlobalRepository repository) async {
+    print("submitting...");
+    String email = _emailController.text;
+    await repository.signUpUser(email);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final globalRepository = Provider.of<GlobalRepository>(context);
+
     return Scaffold(
-      backgroundColor: Colors.orange,
       body: Container(
-        child: Text("Login"),
+        padding: EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF2979FF),
+              Color(0xFF01579B),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 60.0,
+              child: Icon(Icons.person, size: 60.0, color: Colors.white),
+              backgroundColor: Colors.grey[300],
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                _submitForm(globalRepository);
+              },
+              child: Text('ENTRAR'),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 15.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
